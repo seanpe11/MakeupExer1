26de9db78592419ff63ddda8b97784147cdb8e91
seanpe11 <sean.m.s.pe@gmail.com>
2021-06-04 05:33:26 +0800

    initial

---
+++ app/src/main/java/com/mobdeve/pes15/makeupe1/Splash.java
@@ -0,0 +1,35 @@
+package com.mobdeve.pes15.makeupe1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+// copy pasted from https://stackoverflow.com/questions/5486789/how-do-i-make-a-splash-screen
+public class Splash extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreen);
+        ImageView logo = findViewById(R.id.imageSplash);
+        logo.setImageResource(R.drawable.gcash_logo); // was weird when defining in layout.xml
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
