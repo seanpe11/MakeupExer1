87b579b146848ea94c6691c7399ff709c11d55fa
seanpe11 <sean.m.s.pe@gmail.com>
2021-06-04 06:00:37 +0800

    added welcome screen and set listeners for buttons

---
+++ app/src/main/java/com/mobdeve/pes15/makeupe1/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.mobdeve.pes15.makeupe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button register_btn = findViewById(R.id.register_btn);
+        Button login_btn = findViewById(R.id.login_btn);;
+
+        // put in your own number
+        register_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        // set with premade number
+        login_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
