511101bf1381a173d36b819f48fd6e13060ea1f7
seanpe11 <sean.m.s.pe@gmail.com>
2021-06-04 07:47:36 +0800

    added main menu, images not filled out, can go to cashout activity, todo cashout activity

---
+++ app/src/main/java/com/mobdeve/pes15/makeupe1/CashOutActivity.java
@@ -0,0 +1,16 @@
+package com.mobdeve.pes15.makeupe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CashOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash_out);
+        getSupportActionBar().hide();
+
+    }
+}
--- app/src/main/java/com/mobdeve/pes15/makeupe1/MainActivity.java
+++ app/src/main/java/com/mobdeve/pes15/makeupe1/MainActivity.java
@@ -2,7 +2,10 @@ package com.mobdeve.pes15.makeupe1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        LinearLayout cashout_ll = findViewById(R.id.cashout_ll);
+        cashout_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CashOutActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
